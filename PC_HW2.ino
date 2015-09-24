92cd704a6942762753077fa82cf2a3feaf2fa0d6
yuchien <yc2257@cornell.edu>
2015-09-23 21:56:26 -0400

    Finish displaying digits through setting global variable 'digit' to 0~9

--- PC_HW2.ino
+++ PC_HW2.ino
@@ -1,57 +1,181 @@
-/*
-  Button
 
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
-
-
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
-
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
-
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
- */
-
-// constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int buttonPin = 2;
+const int strobePin = 3;
+const int dataPin = 4;
+const int cpPin = 5;
+const int oePin = 6;
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+const int c0Pin = 8;
+const int c1Pin = 9;
+const int c2Pin = 10;
+const int c3Pin = 11;
+const int c4Pin = 12;
+
+const int ledPin =  13;
+
+// global state:
+int buttonState = 0;
+int row_idx = 0;
+
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  
+  pinMode(c0Pin, OUTPUT);
+  pinMode(c1Pin, OUTPUT);
+  pinMode(c2Pin, OUTPUT);
+  pinMode(c3Pin, OUTPUT);
+  pinMode(c4Pin, OUTPUT);
+
+  pinMode(strobePin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(cpPin, OUTPUT);
+  pinMode(oePin, OUTPUT);
+  
   Serial.begin(9600);
+  
+  digitalWrite(c0Pin, LOW);
+  digitalWrite(c1Pin, LOW);
+  digitalWrite(c2Pin, LOW);
+  digitalWrite(c3Pin, LOW);
+  digitalWrite(c4Pin, LOW);
+  
+  digitalWrite(strobePin, HIGH);
+  digitalWrite(dataPin, LOW);
+  digitalWrite(cpPin, HIGH);
+  digitalWrite(oePin, HIGH);
+
 }
+
+
+
+int digits[10][7][5] = {{{0,0,0,0,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,0,0,0,0}},
+                         
+                        {{1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0}},
+
+                        {{0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {0,0,0,0,0},
+                         {0,1,1,1,1},
+                         {0,1,1,1,1},
+                         {0,0,0,0,0}},
+
+                        {{0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {0,0,0,0,0}},
+
+                        {{0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0}},
+
+                        {{0,0,0,0,0},
+                         {0,1,1,1,1},
+                         {0,1,1,1,1},
+                         {0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {0,0,0,0,0}},
+
+                        {{0,0,0,0,0},
+                         {0,1,1,1,1},
+                         {0,1,1,1,1},
+                         {0,0,0,0,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,0,0,0,0}},
+
+                        {{0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0}},
+
+                        {{0,0,0,0,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,0,0,0,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,0,0,0,0}},
+
+                        {{0,0,0,0,0},
+                         {0,1,1,1,0},
+                         {0,1,1,1,0},
+                         {0,0,0,0,0},
+                         {1,1,1,1,0},
+                         {1,1,1,1,0},
+                         {0,0,0,0,0}}};
+
+int digit = 0;
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
+  
+  
+  
+  if(digitalRead(cpPin) == LOW) {
+    
+    // prepare for clock to trigger
+    if(row_idx == 0){
+      digitalWrite(dataPin, HIGH);
+    } else {
+      digitalWrite(dataPin, LOW);
+    }
+
+    delay(1); // to make sure shift rigesture get it's value
+    
+    digitalWrite(c0Pin, digits[digit][row_idx][0]);
+    digitalWrite(c1Pin, digits[digit][row_idx][1]);
+    digitalWrite(c2Pin, digits[digit][row_idx][2]);
+    digitalWrite(c3Pin, digits[digit][row_idx][3]);
+    digitalWrite(c4Pin, digits[digit][row_idx][4]);
+    
+    digitalWrite(cpPin, HIGH);
+
+    row_idx = (row_idx+1) % 7;
+    
+  } else {
+    digitalWrite(cpPin, LOW);
   }
-  else {
-    // turn LED off:
+  
+   if (Serial.available() > 0) {
+      int incomingByte = Serial.read();
+      digit = incomingByte - '0';      
+  }
+
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
     digitalWrite(ledPin, LOW);
   }
 }
